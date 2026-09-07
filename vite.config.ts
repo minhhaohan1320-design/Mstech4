ea2f9d91504af4350b89f8b9dd449c35d09294b4
minhhaohan1320-design <minhhaohan1320@gmail.com>
2026-09-07 16:56:53 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/Mstech_1/',
+    base: '/Mstech4/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
